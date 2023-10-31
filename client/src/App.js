84abec838c93806ed5d8e4a62d43f342486d8845
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2023-10-31 22:26:44 +0100

    commit ready

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {useEffect, useState} from 'react'
+  
+const App = () => {
+
+  const [data, setData] = useState()
+  
+  useEffect(() => {
+    fetch('/api')
+      .then(res => res.json())
+      .then(data => setData(data))
+  }, [])
+  
+  return (
+    <div className="min-h-screen w-full bg-gray-400 flex flex-col justify-center items-center">
+      <section className='text-4xl text-bold '>
+        <p>Hallo Node server for Express</p>
+      </section>
+      <section className='mt-8 text-red-500 text-2xl font-medium' >
+          <p>{!data ? "Loading..." : data.users}</p> 
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/api', (req, res) => {
+    res.json({"users":['userOne', 'userTwo', 'userThree', 'userFour']})
+    
+})
+
+app.listen(5000,() => {
+    console.log('server is started on port 5000')
+})
